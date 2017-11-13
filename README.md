dda6a8f17ded08e9f245fd60cd519c245e2914a1
Puneet Bharti <puneet.bharti@paytm.com>
2017-11-14 04:05:32 +0530

    Update  Offset file and configuration for checking last read line in log files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # logforwarder
+
+####Technologies used
+* Python: for log forwarding 
+* Nodejs: Apis for input and output 
+
+#### local setup nodejs 
+* go to the directory /api
+* execute npm install 
+* execute node app.js
+
+#### Python setup 
+* 
+
+
+
--- forwarder/forwarder.py
+++ forwarder/forwarder.py
@@ -15,7 +15,6 @@ import datetime
 import requests
 from pygrok import Grok
 
-
 config = {
     "host":"http://localhost:3000/save/",
     "paths":["travel.log", "grok.log"],
@@ -24,6 +23,7 @@ config = {
     "app_name": "travel"
 }
 
+config["offset_file"]="forwarder.offset"
 
 def is_json(myjson):
     try:
@@ -37,9 +37,9 @@ def fetchLogFile():
         output_filename = os.path.normpath(file)
         with open(output_filename, "r") as in_file:
             for line in in_file:
-                formatLog(line)
+                formatLog(file, line)
 
-def formatLog(line):
+def formatLog(path, line):
     formattedLine = {}
 
     if(config['type'] == 'json'):
@@ -62,16 +62,49 @@ def formatLog(line):
     formattedLine["@version"] ='v1'
     formattedLine["app"] = config["app_name"]
 
-    postLogOutput(json.dumps(formattedLine))
+    postLogOutput(path, json.dumps(formattedLine))
 
 
-def postLogOutput(postData):
+def postLogOutput(path, postData):
     response = requests.post(config['host'], json=json.loads(postData))
     if(response.status_code != 200):
         raise Exception("Unable to send the data")
     else:
+        print(path)
+        update_offset(path)
         return True
 
 
 
+def set_offset(fileObject):
+    print(fileObject)
+    f = open(config["offset_file"], 'w')
+    f.write(fileObject)
+
+def create_offset():
+    list = []
+    for file in config['paths']:
+        dict = {}
+        dict['file']= str(file)
+        dict['offset']=1
+        list.append(dict)
+    set_offset(str(list))
+
+def update_offset(path):
+    if not is_non_zero_file(config['offset_file']):
+        create_offset()
+    else:
+        output_filename = os.path.normpath(config['offset_file'])
+        with open(output_filename, "r") as in_file:
+            offsets = eval(in_file.readline())
+            for offset in offsets:
+                if(offset['file']==path):
+                    offset['offset']+=1;
+            set_offset(str(offsets))
+
+def is_non_zero_file(fpath):
+    return os.path.isfile(fpath) and os.path.getsize(fpath) > 0
+
 fetchLogFile()
+#create_offset()
+#update_offset("travel.log")
